7c3ed8f44a5dad4e826490f1dc52f8a3257f6c3e
William Muir <wamuir@gmail.com>
2022-10-27 17:29:03 -0500

    Remove uncompress flag on tar extract

--- .github/workflows/update-bindings.sh
+++ .github/workflows/update-bindings.sh
@@ -6,7 +6,7 @@ bazelisk build //tools/graft_package:go_bindings
 
 cd "$(git rev-parse --show-toplevel)" \
   && rm -rf tensorflow \
-  && tar xz --strip-components=5 \
+  && tar x --strip-components=5 \
      -f bazel-bin/tools/graft_package/go_bindings.tar
 
 bazelisk run //:gazelle
