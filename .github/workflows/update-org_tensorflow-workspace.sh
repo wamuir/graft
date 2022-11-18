d01a7b3db8a81bedd2315b101d546c45d56f5500
William Muir <wamuir@gmail.com>
2022-11-18 06:12:06 -0600

    Include tsl proto patch in libtensorflow workspace

--- .github/workflows/update-org_tensorflow-workspace.sh
+++ .github/workflows/update-org_tensorflow-workspace.sh
@@ -26,7 +26,7 @@ http_archive() {
         name = "${1}",
         build_file = "${2}",
         patch_args = [${3:+\"${3}\"}],
-        patches = [${4:+\"${4}\"}],
+        patches = ${4},
         strip_prefix = "${5}",
         urls = ["${6}"],
         sha256 = "${HASH}",
@@ -42,7 +42,6 @@ def tf_repositories():
 EOF
 
 cat <<EOF >> "${WORKSPACE}"
-
     ###########################################################################
     # libtensorflow
     ###########################################################################
@@ -51,21 +50,21 @@ EOF
 http_archive "libtensorflow_linux_x86_64_cpu" \
     "@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow:libtensorflow.BUILD" \
     "-p1" \
-    "" \
+    "[]" \
     "" \
     "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-${TF_VERSION}.tar.gz"
 
 http_archive "libtensorflow_linux_x86_64_gpu" \
     "@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow:libtensorflow.BUILD" \
     "-p1" \
-    "" \
+    "[]" \
     "" \
     "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-gpu-linux-x86_64-${TF_VERSION}.tar.gz"
 
 http_archive "libtensorflow_macos_x86_64_cpu" \
     "@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow:libtensorflow.BUILD" \
     "-p1" \
-    "" \
+    "[]" \
     "" \
     "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-darwin-x86_64-${TF_VERSION}.tar.gz"
 
@@ -79,21 +78,30 @@ EOF
 http_archive "libtensorflow_proto_linux_x86_64_cpu" \
     "@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow_proto:libtensorflow_proto.BUILD" \
     "-p1" \
-    "@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow_proto:libtensorflow_proto.patch" \
+    "[
+            \"@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow_proto:libtensorflow_proto.patch\",
+            \"@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow_proto:libtensorflow_proto_tsl.patch\",
+        ]" \
     "" \
     "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow_proto-${TF_VERSION}.zip"
 
 http_archive "libtensorflow_proto_linux_x86_64_gpu" \
     "@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow_proto:libtensorflow_proto.BUILD" \
     "-p1" \
-    "@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow_proto:libtensorflow_proto.patch" \
+    "[
+            \"@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow_proto:libtensorflow_proto.patch\",
+            \"@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow_proto:libtensorflow_proto_tsl.patch\",
+        ]" \
     "" \
     "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow_proto-${TF_VERSION}.zip"
 
 http_archive "libtensorflow_proto_macos_x86_64_cpu" \
     "@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow_proto:libtensorflow_proto.BUILD" \
     "-p1" \
-    "@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow_proto:libtensorflow_proto.patch" \
+    "[
+            \"@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow_proto:libtensorflow_proto.patch\",
+            \"@com_github_wamuir_graft//third_party/org_tensorflow/libtensorflow_proto:libtensorflow_proto_tsl.patch\",
+        ]" \
     "" \
     "https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow_proto-${TF_VERSION}.zip"
 
@@ -107,7 +115,7 @@ EOF
 http_archive "tensorflow_base_api_def" \
     "@com_github_wamuir_graft//third_party/org_tensorflow/tensorflow_base_api_def:tensorflow_base_api_def.BUILD" \
     "-p1" \
-    "" \
+    "[]" \
     "tensorflow-${TF_VERSION}/tensorflow/core/api_def/base_api" \
     "https://github.com/tensorflow/tensorflow/archive/refs/tags/v${TF_VERSION}.tar.gz"
 
@@ -121,6 +129,6 @@ EOF
 http_archive "tensorflow_go" \
     "@com_github_wamuir_graft//third_party/org_tensorflow/tensorflow_go:tensorflow_go.BUILD" \
     "-p1" \
-    "" \
+    "[]" \
     "tensorflow-${TF_VERSION}/tensorflow/go" \
     "https://github.com/tensorflow/tensorflow/archive/refs/tags/v${TF_VERSION}.tar.gz"
